d3001184b792cdcb0b4ce6ea8c92e88f64a9758d
AymanAkashi <aaggoujj@student.1337.ma>
2023-02-13 11:53:22 +0100

    feat : change variable into Const Variable

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 09:57:02 by aaggoujj          #+#    #+#             */
-/*   Updated: 2023/01/21 09:57:41 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:50:40 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 
 # include <iostream>
 
-template <typename T, size_t N>
-void iter(T(&arr)[N], size_t len, void (*func)(T&)) {
-    if (len > N) {
-        throw std::invalid_argument("len is greater than array size");
-    }
+template <typename T> void iter(T *arr, size_t len, void (*func)(const T&)) {
     for (size_t i = 0; i < len; i++) {
         func(arr[i]);
     }
 }
+
+// template <typename T, size_t N>
+// void iter(T(&arr)[], size_t len, void (*func)(T&)) {
+//     if (len > N) {
+//         throw std::invalid_argument("len is greater than array size");
+//     }
+//     for (size_t i = 0; i < len; i++) {
+//         func(arr[i]);
+//     }
+// }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,32 +6,60 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 09:58:54 by aaggoujj          #+#    #+#             */
-/*   Updated: 2023/01/21 10:03:16 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:51:59 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+// #include <vector>
 
-void square(int &n) {
-	n *= n;
+void square(int const &n) {
+	std::cout << n * n << std::endl;
 }
 
-int main() {
+
+void reverse(std::string const &str) {
+	std::string tmp = str;
+	for (size_t i = 0; i < str.length(); i++) {
+		std::cout << tmp[str.length() - i - 1];
+	}
+	std::cout << std::endl;
+}
+
+
+
+int main()
+{
 	int arr[] = {1, 2, 3, 4, 5};
 	std::cout << "Before:" << std::endl;
 	for (int i = 0; i < 5; i++)
 		std::cout << arr[i] << std::endl;
-	try
-	{
-		iter(arr, 5, square);
-	} catch (std::invalid_argument &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
 	std::cout << "After:" << std::endl;
-	for (int i = 0; i < 5; i++) {
-		std::cout << arr[i] << std::endl;
-	}
+	try
+		{
+			iter(arr, 5, square);
+		}
+	catch (std::invalid_argument &e)
+		{
+			std::cerr << e.what() << std::endl;
+			return 1;
+		}
+//******************************/String/***************************************
+	std::cout << std::endl;
+	std::string str[] = {"Hello", "World", "!"};
+	std::cout << "Before:" << std::endl;
+	for (int i = 0; i < 3; i++)
+		std::cout << str[i] << std::endl;
+	std::cout << "After:" << std::endl;
+	try
+		{
+			iter(str, 3, reverse);
+		}
+	catch (std::invalid_argument &e)
+		{
+			std::cerr << e.what() << std::endl;
+			return 1;
+		}
+
 	return 0;
 }
